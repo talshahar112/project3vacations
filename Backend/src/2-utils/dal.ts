6d99371f8a7ad3aaa9925f5d9102c8fefe7ed2bd
talshahar112 <talshahar112@gmail.com>
2024-12-04 21:08:14 +0200

    organize

--- Backend/src/2-utils/dal.ts
+++ Backend/src/2-utils/dal.ts
@@ -1,8 +1,6 @@
 import mongoose from "mongoose"; // npm i mongoose
 import { appConfig } from "./app-config";
-// DAL = Data Access Layer - The only one accessing the database.
 class DAL {
-    // We need to call this function once:
     public async connect() {
         try {
             const db = await mongoose.connect(appConfig.mongodbConnectionString);
